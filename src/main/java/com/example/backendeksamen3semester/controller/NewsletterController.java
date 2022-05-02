bacb347ebf53d7b56b91479b9113be01f2d6e580
Zerkia <lordzerkia@gmail.com>
2022-05-02 13:37:14 +0200

    initial commit

---
+++ src/main/java/com/example/backendeksamen3semester/controller/NewsletterController.java
@@ -0,0 +1,39 @@
+package com.example.backendeksamen3semester.controller;
+
+import com.example.backendeksamen3semester.model.Newsletter;
+import com.example.backendeksamen3semester.repository.NewsletterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8080")
+public class NewsletterController {
+
+    @Autowired
+    NewsletterRepository newsletterRepository;
+
+    @GetMapping("/sendnewsletter")
+    public List<Newsletter> getAllMails(){ return newsletterRepository.findAll(); }
+
+
+    @PostMapping("/createemail")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Newsletter createEmail(@RequestBody Newsletter newsletter){
+        System.out.println("Email has been added to our newsletter list! :)");
+        return newsletterRepository.save(newsletter);
+    }
+
+    @DeleteMapping("/deleteemail/{newsletterid}")
+    public ResponseEntity<String> deleteEmail(@PathVariable Integer newsletterid) {
+        try {
+            newsletterRepository.deleteById(newsletterid);
+            return new ResponseEntity<>("Email med id " + newsletterid + " er blevet slettet", HttpStatus.OK);
+        } catch (Exception err){
+            return new ResponseEntity<>("Email med id " + newsletterid + " kunne ikke slettes", HttpStatus.NOT_FOUND);
+        }
+    }
+}
