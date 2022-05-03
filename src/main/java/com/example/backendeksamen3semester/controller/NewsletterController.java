0addf32ce9a7e003d4e707d6904662ac188bf87a
Zerkia <lordzerkia@gmail.com>
2022-05-03 14:57:20 +0200

    full crud, unit tests added, finished

--- src/main/java/com/example/backendeksamen3semester/controller/NewsletterController.java
+++ src/main/java/com/example/backendeksamen3semester/controller/NewsletterController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8080")
@@ -25,6 +26,19 @@ public class NewsletterController {
     public Newsletter createEmail(@RequestBody Newsletter newsletter){
         System.out.println("Email has been added to our newsletter list! :)");
         return newsletterRepository.save(newsletter);
+    }
+
+    @PutMapping("/updateemail/{newsletterid}")
+    public ResponseEntity<Newsletter> updateEmail(@PathVariable Integer newsletterid, @RequestBody Newsletter newsletter){
+        Optional<Newsletter> optEmail = newsletterRepository.findById(newsletterid);
+        if (optEmail.isPresent()){
+            newsletterRepository.save(newsletter);
+            return new ResponseEntity<Newsletter>(newsletter, HttpStatus.OK);
+        } else {
+            Newsletter notFound = new Newsletter();
+            notFound.setEmail("Email med id " + newsletterid + " kunne ikke findes");
+            return new ResponseEntity<Newsletter>(notFound, HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/deleteemail/{newsletterid}")
