608e9e6e0b67699061d0f611d962249fc9df7bca
MadsHaderup <mads@haderup.net>
2022-05-05 17:12:32 +0200

    Implemented GalleriService class

--- src/main/java/com/example/backendeksamen3semester/controller/GalleriController.java
+++ src/main/java/com/example/backendeksamen3semester/controller/GalleriController.java
@@ -3,48 +3,52 @@ package com.example.backendeksamen3semester.controller;
 import com.example.backendeksamen3semester.Utils.ImageUtility;
 import com.example.backendeksamen3semester.model.Galleri;
 import com.example.backendeksamen3semester.repository.GalleriRepository;
+import com.example.backendeksamen3semester.service.GalleriService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @CrossOrigin
-public class GalleriRESTController {
+public class GalleriController {
 
     @Autowired
     GalleriRepository galleriRepository;
 
+    @Autowired
+    GalleriService galleriService;
     // Metode fra https://www.techgeeknext.com/spring-boot/spring-boot-upload-image
     @PostMapping("/upload/image")
-    public ResponseEntity<ImageUploadResponse> uplaodImage(@RequestParam("image") MultipartFile file)
+    public ResponseEntity<ImageUploadResponse> uploadImage(@RequestParam("image") MultipartFile file)
             throws IOException {
 
-        galleriRepository.save(Galleri.builder()
-                .name(file.getOriginalFilename())
-                .type(file.getContentType())
-                .image(ImageUtility.compressImage(file.getBytes())).build());
+        galleriService.uploadImg(file);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ImageUploadResponse("Image uploaded successfully: " +
                         file.getOriginalFilename()));
     }
 
     @GetMapping("/galleri")
-    public List<Galleri> getAllMovies() {
-        return galleriRepository.findAll();
+    public List<String> getAllImageNames() {
+        return galleriService.getAll();
+
     }
 
     // Metode fra https://www.techgeeknext.com/spring-boot/spring-boot-upload-image
     @GetMapping(path = {"/get/image/{name}"})
     public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
 
-        final Optional<Galleri> dbImage = galleriRepository.findByName(name);
+        final Optional<Galleri> dbImage = galleriService.getImg(name);
 
         return ResponseEntity
                 .ok()
